4d005329c4330744a2f8d2fa1ebc0ce645fc2b29
Rasmus Kibshede <2740ralle@gmail.com>
2024-04-15 09:41:10 +0200

    setup

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Nest.js example')
+    .setDescription('This is example for nest.js')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.get('RMQ_URL') as string],
+      queue: configService.get('RMQ_QUEUE'),
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  setupSwagger(app);
+  await app.listen(configService.get('APP_PORT'));
+}
+
+bootstrap();
