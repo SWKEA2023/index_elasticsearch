b4909071bf0d1d280fc34b8fa880ffe568ba92d8
Rasmus Kibshede <2740ralle@gmail.com>
2024-04-15 14:16:08 +0200

    added: connection to local docker

--- src/Application/Search/search.module.ts
+++ src/Application/Search/search.module.ts
@@ -5,12 +5,24 @@ import { CommandHandlers } from './Commands/Handlers';
 import { QueryHandlers } from './Queries/Handlers';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import * as fs from 'fs';
 
 @Module({
   imports: [
     CqrsModule,
     ElasticsearchModule.register({
-      node: 'http://localhost:9200',
+      node: 'https://localhost:9200',
+      auth: {
+        // Books API key
+        // apiKey: 'MDRndzNvNEJGOHVRUnBIdDNwQ3A6bHZpRm1oMXRTZ1dkV20yM1d1QVhJdw=='
+
+        // Movies API key
+        apiKey: 'MzRnNTNvNEJGOHVRUnBIdDZaRG86eHBrRkZISVhTQnFnT1dOcUMwaFV4Zw==',
+      },
+      tls: {
+        ca: fs.readFileSync('http_ca.crt'),
+        rejectUnauthorized: false,
+      },
     }),
   ],
   providers: [SearchService, ...QueryHandlers, ...CommandHandlers],
